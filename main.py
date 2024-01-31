5f58a98e6965fd84664afca3faa0b32c6723164d
JBSosa <juanbsosa01@gmail.com>
2024-01-30 19:48:30 -0600

    Day2

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ async def root():
 
 @app.get("/developer/{desarrollador}")
 def developer(desarrollador: str):
-  steamGamesData=pd.read_csv('steamGamesDf.csv')
+  steamGamesData=pd.read_csv('steamGames.csv')
   steamGamesDf=pd.DataFrame(steamGamesData)
   filteredDf=steamGamesDf[steamGamesDf['developer']==desarrollador]
   totalApps=filteredDf.groupby('Año').size().reset_index(name='Cantidad de Items')
@@ -22,9 +22,31 @@ def developer(desarrollador: str):
   result=result.to_dict(orient='records')
   return JSONResponse(content=result)
 
-@app.get("/userdata")
-def userdata(User_id: str): 
-  return
+@app.get("/userdata/{userId}")
+def userdata(userId: str):
+  #Cargar CSV
+  userRecommendData=pd.read_csv('userReviewsExploded.csv')
+  userItemsData=pd.read_csv('userItemsExploded.csv')
+  userItemCountData=pd.read_csv('userItemCount.csv')
+  gamePriceData=pd.read_csv('steamGamesPrice.csv')
+  #Convertir a DataFrame
+  userRecommendDf=pd.DataFrame(userRecommendData)
+  userItemsDf=pd.DataFrame(userItemsData)
+  userItemCountDf=pd.DataFrame(userItemCountData)
+  gamePriceDf=pd.DataFrame(gamePriceData)
+  #Filtrar por usuario
+  userRecommendDf=userRecommendDf[userRecommendDf['user_id']==userId].reset_index(drop=True)
+  userItemsDf=userItemsDf[userItemsDf['user_id']==userId].reset_index(drop=True)
+  userItemsDf=pd.merge(userItemsDf,gamePriceDf,how='left',left_on='item_id',right_on='id')
+  userItemCountDf=userItemCountDf[userItemCountDf['user_id']==userId].reset_index(drop=True)
+  #Componer DataFrame de respuesta
+  resultData={'Usuario': [userId]}
+  resultDf=pd.DataFrame(resultData)
+  resultDf['Dinero Gastado']=pd.to_numeric(userItemsDf['price'], errors='coerce').sum()
+  resultDf['% de recomendación']=(userRecommendDf['recommend'].mean()) * 100
+  resultDf['Cantidad de Items']=userItemCountDf['items_count']
+  result=resultDf.to_dict(orient='records')
+  return JSONResponse(content=result)
 
 @app.get("/userforgenre")
 def UserForGenre(genero: str):
