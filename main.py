f9695e1cbf9e2390624bc460ee00597a87fda3bb
Juan Sosa <juanbsosa01@gmail.com>
2024-02-06 00:05:30 -0600

    main.py optimizacion

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ async def root():
 @app.get("/developer/{dev}")
 def developer(dev: str):
     # Se leen los datos y se crea el DataFrame
-    steamGamesData = pd.read_parquet('data/steamGames.parquet')
-    steamGamesDf = pd.DataFrame(steamGamesData)
+    steamGamesDf = pd.read_parquet('data/steamGames.parquet')
     # Se crea un DataFrame filtrado de acuerdo al developer
     filteredDf = steamGamesDf[steamGamesDf['developer'].str.lower() == dev.lower()]
     # Se guardan los valores del total de juegos y la cantidad de juegos gratuitos agrupados por año
@@ -31,14 +30,10 @@ def developer(dev: str):
 @app.get("/userdata/{userId}")
 def userData(userId: str):
     # Se leen los datos y se crea el DataFrame
-    userRecommendData = pd.read_parquet('data/userReviewsExploded.parquet')
-    userItemsData = pd.read_parquet('data/userItemsExploded.parquet')
-    userItemCountData = pd.read_parquet('data/userItemCount.parquet')
-    steamGamesPriceData = pd.read_parquet('data/steamGamesPrice.parquet')
-    userRecommendDf = pd.DataFrame(userRecommendData)
-    userItemsDf = pd.DataFrame(userItemsData)
-    userItemCountDf = pd.DataFrame(userItemCountData)
-    steamGamesPriceDf = pd.DataFrame(steamGamesPriceData)
+    userRecommendDf = pd.read_parquet('data/userReviewsExploded.parquet')
+    userItemsDf = pd.read_parquet('data/userItemsExploded.parquet')
+    userItemCountDf = pd.read_parquet('data/userItemCount.parquet')
+    steamGamesPriceDf = pd.read_parquet('data/steamGamesPrice.parquet')
     # Se crean DataFrame filtrando por usuario
     userRecommendDf = userRecommendDf[userRecommendDf['user_id'] == userId].reset_index(drop = True)
     userItemsDf = userItemsDf[userItemsDf['user_id'] == userId].reset_index(drop = True)
@@ -58,37 +53,33 @@ def userForGenre(genre: str):
     # Se transfroma la variable a minúscula para poder realizar la búsqueda sin preocuparse por la capitalización
     genre = genre.lower()
     # Se leen los datos y se crea el DataFrame
-    steamGamesGenresData = pd.read_parquet('data/steamGamesGenres.parquet')
-    userItemsExplodedData = pd.read_parquet('data/userItemsExploded.parquet')
-    steamGamesGenreDf = pd.DataFrame(steamGamesGenresData)
-    userItemsExplodedDf = pd.DataFrame(userItemsExplodedData)
+    steamGamesGenresDf = pd.read_parquet('data/steamGamesGenres.parquet')
+    userItemsExplodedDf = pd.read_parquet('data/userItemsExploded.parquet')
     # Se transforma igualmente la columna de 'genres' del DataFrame a minúscula
-    steamGamesGenreDf['genres'] = steamGamesGenreDf['genres'].apply(lambda lst: [element.lower() for element in lst])
+    steamGamesGenresDf['genres'] = steamGamesGenresDf['genres'].apply(lambda lst: [element.lower() for element in lst])
     # Se filtra el DataFrame buscando el género solicitado entre los géneros del juego
-    steamGamesGenreDf = steamGamesGenreDf[steamGamesGenreDf['genres'].apply(lambda x: genre in x if isinstance(x, list) else False)]
+    steamGamesGenresDf = steamGamesGenresDf[steamGamesGenresDf['genres'].apply(lambda x: genre in x if isinstance(x, list) else False)]
     # Se une el DataFrame con los géneros al DataFrame que contiene a los usuarios con sus ítems
-    steamGamesGenreDf = pd.merge(steamGamesGenreDf[['item_id','release_year']],userItemsExplodedDf,how = 'left',on = 'item_id')
+    steamGamesGenresDf = pd.merge(steamGamesGenresDf[['item_id','release_year']],userItemsExplodedDf,how = 'left',on = 'item_id')
     # Se descartan todas las instancias que no contengan tiempo de juego
-    steamGamesGenreDf = steamGamesGenreDf.dropna(subset = 'playtime')
+    steamGamesGenresDf = steamGamesGenresDf.dropna(subset = 'playtime')
     # Se agrupan de acuerdo al ID de usuario teniendo en cuenta el tiempo total de juego y se recupera al usuario que posee mayor cantidad de tiempo acumulado
-    userId = steamGamesGenreDf.groupby('user_id')['playtime'].sum().reset_index().sort_values(by = 'playtime',ascending = False).iloc[0,0]
+    userId = steamGamesGenresDf.groupby('user_id')['playtime'].sum().reset_index().sort_values(by = 'playtime',ascending = False).iloc[0,0]
     # Se filtra el DataFrame de acuerdo al usuario recuperado, se agrupa conforme al año de lanzamiento de los juegos y se suma el tiempo jugado
-    steamGamesGenreDf = steamGamesGenreDf[steamGamesGenreDf['user_id'] == userId].groupby('release_year')['playtime'].sum().reset_index().sort_values(by = 'release_year',ascending = False)
+    steamGamesGenresDf = steamGamesGenresDf[steamGamesGenresDf['user_id'] == userId].groupby('release_year')['playtime'].sum().reset_index().sort_values(by = 'release_year',ascending = False)
     # Una vez se tiene toda la información, se transforman los datos y se crea el DataFrame de respuesta
-    steamGamesGenreDf['playtime'] = (steamGamesGenreDf['playtime']/60).round(2)
-    steamGamesGenreDf.rename(columns = {'release_year': 'Año'}, inplace = True)
-    steamGamesGenreDf.rename(columns = {'playtime': 'Horas jugadas'}, inplace = True)
-    resultPlaytime = steamGamesGenreDf.to_dict(orient = 'records')
+    steamGamesGenresDf['playtime'] = (steamGamesGenresDf['playtime']/60).round(2)
+    steamGamesGenresDf.rename(columns = {'release_year': 'Año'}, inplace = True)
+    steamGamesGenresDf.rename(columns = {'playtime': 'Horas jugadas'}, inplace = True)
+    resultPlaytime = steamGamesGenresDf.to_dict(orient = 'records')
     result = {'Usuario con más horas jugadas para género ' + genre:userId,'Horas':resultPlaytime}
     return JSONResponse(content = result)
 
 @app.get("/bestdeveloperyear/{year}")
 def bestDeveloperYear(year: int):
     # Se cargan los datos y se crean los DataFrame
-    userReviewsExplodedData = pd.read_parquet('data/userReviewsExploded.parquet')
-    steamGamesDevData = pd.read_parquet('data/steamGamesDev.parquet')
-    userReviewsExplodedDf = pd.DataFrame(userReviewsExplodedData)
-    steamGamesDevDf = pd.DataFrame(steamGamesDevData)
+    userReviewsExplodedDf = pd.read_parquet('data/userReviewsExploded.parquet')
+    steamGamesDevDf = pd.read_parquet('data/steamGamesDev.parquet')
     # Se filtra el DataFrame de acuerdo al año solicitado eliminando los juegos no recomendados, y con análisis de sentimiento neutral o negativo
     userReviewsExplodedDf = userReviewsExplodedDf[(userReviewsExplodedDf['recommend'] == True) & 
                                             (userReviewsExplodedDf['sentiment_analysis'] == '2') & 
@@ -113,10 +104,8 @@ def bestDeveloperYear(year: int):
 @app.get("/developerreviewsanalysis/{dev}")
 def developerReviewsAnalysis(dev: str):
     # Se cargan los datos y se crean los DataFrame
-    userReviewsExplodedData = pd.read_parquet('data/userReviewsExploded.parquet')
-    steamGamesDevData = pd.read_parquet('data/steamGamesDev.parquet')
-    steamGamesDevDf = pd.DataFrame(steamGamesDevData)
-    userReviewsExplodedDf = pd.DataFrame(userReviewsExplodedData)
+    userReviewsExplodedDf = pd.read_parquet('data/userReviewsExploded.parquet')
+    steamGamesDevDf = pd.read_parquet('data/steamGamesDev.parquet')
     # Se filtra el DataFrame de desarrolladores de acuerdo al desarrollador solicitado
     steamGamesDevDf = steamGamesDevDf[steamGamesDevDf['developer'].str.lower() == dev.lower()]
     # Se agrupa el DataFrame de reseñas de acuerdo al ID del juego agregando una columna para la suma de comentarios positivos y otra para la de negativos
@@ -133,8 +122,7 @@ def developerReviewsAnalysis(dev: str):
 @app.get("/recommenditem/{itemId}")
 def recommendItem(itemId: str):
     # Se cargan los datos y se crea el DataFrame
-    itemSimData = pd.read_parquet('data/itemSim.parquet')
-    itemSimDf = pd.DataFrame(itemSimData)
+    itemSimDf = pd.read_parquet('data/itemSim.parquet')
     counter = 1
     result = {'Aquí hay juegos similares a': itemId,'1':'','2':'','3':'','4':'','5':''}
     # Se buscan los ítems con mayor similitud y se añaden al resultado
